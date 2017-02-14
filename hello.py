cf4f10beb505e137df260c9c5c369ed0a0c5baba
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-02-14 01:30:14 -0600

    hello, new route

--- hello.py
+++ hello.py
@@ -14,6 +14,13 @@ def faq():
     # Lo que la función regresa es lo que la ruta devolverá
     return "Trying my own route"
 
+
+@app.route("/echo")
+def echo():
+    response_string = "Recibí: "
+    for k, v in request.iteritems():
+        response_string += "%s: %s," % (k, v) 
+    return response_string
 # No correr accidentalmente este archivo si es importado
 if __name__ == "__main__":
     app.run()
